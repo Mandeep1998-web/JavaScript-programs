c3dc1f64f4ee90707b958971f21fc530ba709467
Mandeep1998-web <57211095+Mandeep1998-web@users.noreply.github.com>
2020-07-23 12:29:53 +0530

    Add files via upload

---
+++ website with bootstrap/script.js
@@ -0,0 +1,6 @@
+ document.querySelector('.myform').addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(event.target.username.value);
+    console.log(event.target.realname.value);
+    event.target.username.value='';
+})
